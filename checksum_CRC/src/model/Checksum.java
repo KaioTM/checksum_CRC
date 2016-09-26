d188871ed596cbdb7cdf664ad913dfd3324e3bca
KaioTM <kaioteixeira@id.uff.br>
2016-09-26 17:52:23 -0300

    Ajustes checksum v2

--- checksum_CRC/src/model/Checksum.java
+++ checksum_CRC/src/model/Checksum.java
@@ -5,14 +5,60 @@
  */
 package model;
 
-import java.util.Random;
-
+import java.util.ArrayList;
+import java.lang.*;
 /**
  *
  * @author kaio.teixeira
  */
 public class Checksum {
-  
-    
-    
+    public String checksum(ArrayList<String> mensagemDigitada){
+        
+        String parcial1="00000000";
+        String parcial2="00000000";
+        String checksum="00000000";
+        String soma="00000000";
+        Binario binario = new Binario();
+//        
+//        //Valor parcial dos bits 2 e 1
+//        System.out.println(mensagemDigitada.get(1)+" "+ mensagemDigitada.get(0));
+//        //Valor parcial dos bits 4 e 3
+//        System.out.println(mensagemDigitada.get(3)+" "+ mensagemDigitada.get(2));
+        
+            for (int i=1;i<=mensagemDigitada.size();){
+                if (mensagemDigitada.size()== 1){
+                    parcial2=mensagemDigitada.get(0);
+                    soma = parcial1 + " " + parcial2;
+                    checksum = soma;
+                    checksum = checksum.replace('0', '2').replace('1', '0').replace('2', '1');
+                    return checksum;
+                }else{
+                    if(mensagemDigitada.size()== 2){
+                        parcial1 = mensagemDigitada.get(1);
+                        parcial2 = mensagemDigitada.get(0);
+                        soma = parcial1 + " " + parcial2;
+                        checksum = soma;
+                        checksum = checksum.replace('0', '2').replace('1', '0').replace('2', '1');
+                        return checksum;
+                    }else{
+                        try{
+                            
+                            
+                        }
+                        catch (IndexOutOfBoundsException ex){
+                            
+                        }
+                        
+                    }
+                }
+                
+//                else{
+//                    parcial1 = binario.somaBinario(mensagemDigitada.get(i+1), mensagemDigitada.get(i));
+//                    parcial2 = binario.somaBinario(mensagemDigitada.get(i+3), mensagemDigitada.get(i+2));
+//                }
+                i=i+2;
+            
+            }
+            return checksum;
+    }
 }
