1c36a4987d8129c0194e84c281c55a91e339c559
KaioTM <kaioteixeira@id.uff.br>
2016-11-28 00:13:00 -0200

    Conversão de Polinomios para binário (Em andamento)
    Ainda falta colocar "0" nos graus faltantes

--- checksum_CRC/src/model/Crc.java
+++ checksum_CRC/src/model/Crc.java
@@ -13,18 +13,73 @@ import java.util.regex.Pattern;
  * @author kaio.teixeira
  */
 public class Crc {
-     public String crc(ArrayList<String> mensagemDigitada, int tamanhoMensagem,String polinomioGerador){ 
-        String crc ="0";
+
+    public String crc(ArrayList<String> mensagemDigitada, int tamanhoMensagem, String polinomioGerador) {
+        String crc = "0";
         Binario binario = new Binario();
         return crc;
-     }
-     public ArrayList <String> criaBinarioPolinomioGerador (String polinomioGeradorDigitado){
-         ArrayList <String> binarioPolinomio = new ArrayList();
-         String caracteres = " #@_\\/.*+Xx";
-         String[] teste;
-         teste = polinomioGeradorDigitado.split("[" + Pattern.quote(caracteres) + "]");
-         
-         
-         return binarioPolinomio;
-     }
+    }
+
+    public ArrayList<String> criaBinarioPolinomioGerador(String polinomioGeradorDigitado) {
+        ArrayList<String> binarioPolinomio = new ArrayList();
+        String caracteres = " #@_\\/.*+";
+        String[] fatores;
+        fatores = polinomioGeradorDigitado.split("[" + Pattern.quote(caracteres) + "]");
+        
+        for (int i = 0; i <= 8; i++) {
+            try{
+            if (fatores[i].contains("X^")) {
+                fatores[i] = fatores[i].replace("X^", "");
+                int fator = Integer.parseInt(fatores[i]);
+                //Parte Responsável por transformar os fatores do polinômio em binário
+                for (int n = 0; n <= fator; n++) {
+                    if (n == (8 - fator)) {
+                        binarioPolinomio.add((8 - fator), "1");
+                        break;
+                    }else {
+                        try {
+                            binarioPolinomio.get(n);
+                        } catch (IndexOutOfBoundsException e) {
+                            binarioPolinomio.add(n, "0");
+                        }
+                    }  
+                }
+            }else if(fatores[i].contains("X")){
+                int fator = 7;
+                for (int n = 0; n <= fator; n++) {
+                    if (n == fator) {
+                        binarioPolinomio.add(7,"1");
+                        break;
+                    }else {
+                        try {
+                            binarioPolinomio.get(n);
+                        } catch (IndexOutOfBoundsException e) {
+                            binarioPolinomio.add(n, "0");
+                        }
+                    }  
+                }  
+            }else if (fatores[i].contains("1")){   
+               int fator = 8;
+                for (int n = 0; n <= fator; n++) {
+                    if (n == fator) {
+                        binarioPolinomio.add(8,"1");
+                        break;
+                    }else {
+                        try {
+                            binarioPolinomio.get(n);
+                        } catch (IndexOutOfBoundsException e) {
+                            binarioPolinomio.add(n, "0");
+                        }
+                    }  
+                }  
+            }
+                       
+        
+    }
+            catch (ArrayIndexOutOfBoundsException c){
+                binarioPolinomio.add(i, "0");
+            }
+        }
+        return binarioPolinomio;
+    }
 }
